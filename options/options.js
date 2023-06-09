1e43e10f914772d60bd0bfe9ec0b5062192aaf46
Santosh Purbey <purbey.santosh@hotmail.com>
2023-06-09 15:00:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "module-intall-v3-16",
+  url: "https://module-intall-v3-16.botics.co",
+  api: "https://module-intall-v3-16.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
